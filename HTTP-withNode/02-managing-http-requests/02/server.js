6197b5ba085e1310a30ce3bbb7222a727831d199
Pavan007 <pavanbm17@gmail.com>
2022-02-18 12:23:57 +0530

    18/2 changes

---
+++ HTTP-withNode/02-managing-http-requests/02/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer();
+server.on('request', (request, response) => {
+    console.log("this is incoming request");
+});
+
+server.listen(8080);
